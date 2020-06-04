0878ec47169d7806f952f616893cb2ac40646e8e
Eugênio Oliveira <eugenio.p.oliveira@outlook.com>
2020-06-04 11:51:32 -0300

    Finalizando a configuraçao inicial do Aplicativo Backend

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,10 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', ()=> {
+app.get('/users', (request, response)=> {
  console.log("Hello, World");
+
+ response.json(["Eugênio", "Pollyana", "Nícolas"]);
 });
 
 app.listen(3333);
