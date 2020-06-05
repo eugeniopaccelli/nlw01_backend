aa945b9a2942ebd8b5e02fdce2a28e70930395f0
Eugênio Oliveira <eugenio.p.oliveira@outlook.com>
2020-06-04 22:42:28 -0300

    criaçao do endpoint de listagem de itens

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,27 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
 routes.get('/', (request, response) => {
-  return response.json({ msg: "Hello, World..."})
- });
+
+  return response.json({ msg: "Bem vindo(a) ao E-Coleta!" })
+
+});
+
+routes.get('/items',  async (request, response) => {
+
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`
+    }
+  })
+
+  return response.json(serializedItems)
+
+});
 
 export default routes;
